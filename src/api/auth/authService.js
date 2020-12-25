b7a5699137cd356452377a1e69667ccaf4186c77
JK <49369341+iJKTen@users.noreply.github.com>
2020-12-25 17:02:00 -0500

    better user model & service

--- src/api/auth/authService.js
+++ src/api/auth/authService.js
@@ -2,29 +2,47 @@
 
 const jwt = require('jsonwebtoken');
 const { config } = require('../../config');
+const bcrypt = require('bcrypt');
+
+const encrypt = async (password) => {
+    const salt = await bcrypt.genSalt(config.crpyto.BCRYPT_WORK_FACTOR);
+    return await bcrypt.hash(password, salt);
+};
 
 const userService = (userModel) => {
     return {
         register: async (registration) => {
-            return await userModel.create(registration);
+            const registrationUser = {
+                email: registration.email,
+                password: encrypt(registration.password)
+            };
+            const user = await userModel.create(registrationUser);
+            return {
+                _id: user._id,
+                email: user.email
+            };
         },
         login: async (login) => {
-            const user = await userModel.login(login);
+            const user = await userModel.get(login.email);
             if (!user) {
                 return new Error('User not found!');
             }
-            const accessToken = jwt.sign(
-                {
-                    id: user.id
-                },
-                config.jwt.JSON_WEB_TOKEN_SECRET,
-                { expiresIn: config.jwt.JSON_WEB_TOKEN_EXPIRATION });
 
-            return {
-                id: user.id,
-                email: user.email,
-                accessToken: accessToken
-            };
+            if (bcrypt.compare(user.password, login.password)) {
+                const accessToken = jwt.sign(
+                    {
+                        id: user.id
+                    },
+                    config.jwt.JSON_WEB_TOKEN_SECRET,
+                    { expiresIn: config.jwt.JSON_WEB_TOKEN_EXPIRATION });
+
+                return {
+                    id: user.id,
+                    email: user.email,
+                    accessToken: accessToken
+                };
+            }
+            return new Error('User not found!');
         }
     };
 };
--- src/api/users/user.js
+++ src/api/users/user.js
@@ -1,21 +1,8 @@
 'use strict';
 
-const { config, dbClient } = require('../../config/');
-const bcrypt = require('bcrypt');
+const { dbClient } = require('../../config/');
 
 const userCollection = 'users';
-
-const encrypt = async (password) => {
-    const salt = await bcrypt.genSalt(config.crpyto.BCRYPT_WORK_FACTOR);
-    return await bcrypt.hash(password, salt);
-};
-
-const replacePwd = (user, password) => {
-    return {
-        ...user,
-        password
-    };
-};
 
 module.exports = {
     index: async () => {
@@ -26,18 +13,21 @@ module.exports = {
         try {
             await client.connect();
             const collection = client.db().collection(userCollection);
-            const encryptedPwd = await encrypt(user.password);
-            const { password_confirmation, ...aUser } = user;
-            const newUser = replacePwd(aUser, encryptedPwd);
-            const result = await collection.insertOne(newUser);
-            const { password, ...newResult } = result.ops[0];
-            return newResult;
+            const result = await collection.insertOne(user);
+            return result.ops[0];
         } finally {
             client.close();
         }
     },
     get: async (email) => {
-
+        const client = dbClient();
+        try {
+            await client.connect();
+            const collection = client.db().collection(userCollection);
+            return await collection.findOne({ email });
+        } catch (err) {
+            client.close();
+        }
     },
     update: async () => {
 
@@ -47,22 +37,5 @@ module.exports = {
     },
     changePassword: async () => {
 
-    },
-    login: async (user) => {
-        const client = dbClient();
-        try {
-            await client.connect();
-            const collection = client.db().collection(userCollection);
-            const dbUser = await collection.findOne({
-                email: user.email
-            });
-            if (bcrypt.compare(user.password, dbUser.password)) {
-                const { password, ...foundUser } = dbUser;
-                return foundUser;
-            }
-            return null;
-        } catch (err) {
-            client.close();
-        }
     }
 };
