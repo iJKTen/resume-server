7fdd6bd6624229a416f4722ad361fc75e0edccd9
JK <49369341+iJKTen@users.noreply.github.com>
2021-01-11 21:33:23 -0500

    added compression and moved cors preflight to its proper place

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const compression = require('compression');
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
@@ -10,17 +11,19 @@ const corsOptions = {
     origin: 'http://localhost:3000'
 };
 
+app.use(compression());
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use('/api', routes);
 
 app.use((req, res, next) => {
     res.header(
         'Access-Control-Allow-Headers',
-        'x-access-token, Origin, Content-Type, Accept'
+        'x-access-token, Origin, Content-Type'
     );
     next();
 });
+
+app.use('/api', routes);
 
 app.use((req, res) => {
     res.status(404).send({ 'message': 'Resource not found' });
