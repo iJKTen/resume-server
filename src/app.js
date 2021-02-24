5c6eeca2c22a1b97a40ddd1d996bf285a324845b
JK <49369341+iJKTen@users.noreply.github.com>
2021-02-23 19:42:40 -0500

    testing again 3

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     );
     res.header(
         'Access-Control-Allow-Origin',
-        req.headers.origin
+        'https://silly-keller-c1e934.netlify.app'
     );
     next();
 });
