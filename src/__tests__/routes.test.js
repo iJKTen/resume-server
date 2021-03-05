3cc8b8cec7a9f44d05290522d6fc2d14217b1fd3
JK <49369341+iJKTen@users.noreply.github.com>
2021-03-05 13:23:38 -0500

    testing routes

--- src/__tests__/routes.test.js
+++ src/__tests__/routes.test.js
@@ -3,6 +3,8 @@
 const request = require('supertest');
 const app = require('../app');
 
+let accessToken = '';
+
 test('login failed', async () => {
     await request(app)
         .post('/api/auth/login')
@@ -11,4 +13,32 @@ test('login failed', async () => {
             password: 'testing'
         })
         .expect(404);
+});
+
+test('login successful', async () => {
+    await request(app)
+        .post('/api/auth/login')
+        .send({
+            email: 'test@test.com',
+            password: 'test'
+        })
+        .expect(200)
+        .expect((res) => {
+            accessToken = res.body.accessToken;
+        });
+});
+
+test('get all resumes return 200', async () => {
+    await request(app)
+        .get('/api/resumes')
+        .set('x-access-token', accessToken)
+        .send()
+        .expect(200);
+});
+
+test('access token missing returns 401', async () => {
+    await request(app)
+        .get('/api/resumes')
+        .send()
+        .expect(401);
 });
