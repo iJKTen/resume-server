dd1f619e4ab926e405d7a80474cf615f308aafe7
JK <49369341+iJKTen@users.noreply.github.com>
2021-03-05 12:59:18 -0500

    testing routes with super test

---
+++ src/__tests__/routes.test.js
@@ -0,0 +1,13 @@
+const { TestScheduler } = require('jest');
+const request = require('supertest');
+const app = require('../app');
+
+test('login failed', async () => {
+    await request(app)
+        .post('/api/auth/login')
+        .send({
+            email: 'test@test.com',
+            password: 'testing'
+        })
+        .expect(404);
+});
